7b1ae474da62e62a165d3d275e21ad2771444749
brlga002 <gabriel.lima1000@gmail.com>
2023-07-11 15:07:11 -0400

    feat: added stacks

---
+++ stacks/UserStack.ts
@@ -0,0 +1,19 @@
+import { StackContext, use } from 'sst/constructs'
+
+import { ApiGatewayStack } from './ApiGatewayStack'
+
+export function UserStack({ stack }: StackContext) {
+  const { api } = use(ApiGatewayStack)
+
+  api.addRoutes(stack, {
+    'POST /users': 'src/4-framework/functions/user/create.handler',
+    'GET /users': 'src/4-framework/functions/user/list.handler',
+    'GET /users/{id}': 'src/4-framework/functions/user/get.handler',
+    'PUT /users/{id}': 'src/4-framework/functions/user/update.handler',
+    'DELETE /users/{id}': 'src/4-framework/functions/user/delete.handler',
+  })
+
+  stack.addOutputs({
+    ApiEndpoint: api.url,
+  })
+}
